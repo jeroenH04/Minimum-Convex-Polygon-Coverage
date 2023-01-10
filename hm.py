a750a36b5a6810880c5fd5f8e14eccd76f1f8696
Jeroen Hellenbrand <j.w.f.hellenbrand@student.tue.nl>
2023-01-10 23:33:08 +0100

    fix(code quality)

--- hm.py
+++ hm.py
@@ -48,15 +48,14 @@ class HertelMehlhorn:
             self.polygons.append(Polygon(t.v, t.edges))
 
         prevScore = 0
+        # Decompose the polygons until there is no improvement on the number of polygons
         while prevScore - len(self.polygons) != 0:
             prevScore = len(self.polygons)
             self.decompose()
 
     def decompose(self):
         # For every triangle:
-        t1 = 0
-        while t1 < len(self.polygons):
-            polygon1 = self.polygons[t1]
+        for polygon1 in self.polygons:
             for i11 in range(polygon1.getNumPoints()):
                 # Set d1 and d2 to first two points of the triangle
                 d1 = polygon1.getPoint(i11)
@@ -154,9 +153,6 @@ class HertelMehlhorn:
                 i11 = -1
 
                 continue
-        
-            # If no new polygon was created, move on to the next one
-            t1 += 1
 
         self.T.polygons = self.polygons
 
